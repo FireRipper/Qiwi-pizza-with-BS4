7442f49d4acfc6420e7827244bd9272d84c42cf2
FireRipper <ilya-shabelnik@mail.ru>
2018-06-05 17:57:31 +0300

    Add function 'form check' for callback page

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,4 @@
+
 /*function plusQuantity(index) {
     const product = document.getElementById('product' + index);
     const quantity = product.getElementsByClassName('quantity')[0];
@@ -72,3 +73,22 @@ $(document).ready(function () {
         })
     });
 });
+
+//Form check
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        let forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        let validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
